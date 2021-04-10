fc56fa667b682396ee473316a0b0cccb50c8d373
SwatishHarikrishnan <80979671+SwatishHarikrishnan@users.noreply.github.com>
2021-04-10 15:22:43 +0530

    Add files via upload

---
+++ day1/testcase/CreateLead.java
@@ -0,0 +1,29 @@
+package week5.day1.testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass {
+	
+	@Test
+	public void runcreateload() 
+	{
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+}
+}
+
+
+
+
+
+
